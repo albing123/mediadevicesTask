93a9058fb1d3a2dfa4f7a8fa739ed81e91e3950c
albing123 <89390005+albing123@users.noreply.github.com>
2022-05-24 11:59:17 +0200

    third commit

--- gallery.js
+++ gallery.js
@@ -18,3 +18,14 @@ function createImage(image) {
 }
 
 getImages()
+
+
+window.addEventListener('load', async () => {
+    if('serviceWorker' in navigator){
+        try {
+            await navigator.serviceWorker.register('service-worker.js');
+        } catch(err) {
+            console.error('Whooopsie!', err)
+        }
+    }
+});
--- index.js
+++ index.js
@@ -3,31 +3,28 @@ let context = canvas.getContext("2d");
 let video = document.querySelector("#video")
 let gallery = document.querySelector("#gallery")
 
-
 let images = [];
-
 
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 
-    navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
+    navigator.mediaDevices.getUserMedia({video: {width: 342, height: 291}}).then(stream => {
         video.srcObject = stream;
         video.play()
     })
 
 }
 
-
-
 document.getElementById("snap").addEventListener("click",() => {
-    context.drawImage(video, 0,0,640, 480);
+    context.drawImage(video, 0,0,342, 291);
     const imageData = canvas.toDataURL("image/png");
 
 
-    
+    document.getElementById("video").style.visibility = "hidden"
+    document.getElementById("newPicture").style.visibility = "visible"
+    document.getElementById("snap").style.visibility = "hidden"
+    document.getElementById("canvas").style.visibility = "visible"
 
-    
-    
-    
+  
     images = JSON.parse(localStorage.getItem("savedImages")) || [];
     images.push({
         id: images.length,
@@ -37,7 +34,24 @@ document.getElementById("snap").addEventListener("click",() => {
 
 });
 
+document.getElementById("newPicture").addEventListener("click",() => {
+
+    document.getElementById("video").style.visibility = "visible"
+    document.getElementById("snap").style.visibility = "visible"
+    document.getElementById("newPicture").style.visibility = "hidden"
+    document.getElementById("canvas").style.visibility = "hidden"
+
+})
 
 
 
 
+window.addEventListener('load', async () => {
+    if('serviceWorker' in navigator){
+        try {
+            await navigator.serviceWorker.register('service-worker.js');
+        } catch(err) {
+            console.error('Whooopsie!', err)
+        }
+    }
+});
